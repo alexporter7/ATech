58917047c6b02911bf72380a4f7ddce247f73fb7
alexporter7 <alex.porter@ucf.edu>
2022-11-26 20:01:00 -0500

    Fixed a bug since i got rid of flywheels

--- src/main/java/com/alexp777/atech/item/ModCreativeModeTab.java
+++ src/main/java/com/alexp777/atech/item/ModCreativeModeTab.java
@@ -1,5 +1,6 @@
 package com.alexp777.atech.item;
 
+import com.alexp777.atech.block.ModBlocks;
 import com.alexp777.atech.util.ModValue;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
@@ -9,7 +10,7 @@ public class ModCreativeModeTab {
 	public static final CreativeModeTab ATECH_TAB = new CreativeModeTab(ModValue.CREATIVE_MODE_TAB_LABEL) {
 		@Override
 		public ItemStack makeIcon() {
-			return new ItemStack(ModItems.IRON_FLY_WHEEL.get());
+			return new ItemStack(ModBlocks.STEEL_FORGE.get());
 		}
 
 	};
